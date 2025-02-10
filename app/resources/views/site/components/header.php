089cfcbfc6680c822feb8b70d26e0f8ea490cf8e
Mr-No-bo-dy <o.m.gutsalyuk@gmail.com>
2025-02-10 19:10:32 +0200

    fix reviews classes

--- app/resources/views/site/components/header.php
+++ app/resources/views/site/components/header.php
@@ -16,6 +16,7 @@
                 <li><a class="head_link" href="home">Home</a></li>
                 <li><a class="head_link" href="categories">Categories</a></li>
                 <li><a class="head_link" href="catalog">Catalogue</a></li>
+                <li><a class="head_link" href="reviews">Reviews</a></li>
                 <li><a class="head_link" href="cart">Cart</a></li>
                 <?php if (!isset($_SESSION['user'])) { ?>
                       <li><a class="head_link" href="register">Register</a></li>
