7c6bfe4493bbbd0114ca0783b9e2136641868f27
Mr-No-bo-dy <o.m.gutsalyuk@gmail.com>
2025-02-17 20:15:35 +0200

    admin role

--- app/resources/views/admin/components/header.php
+++ app/resources/views/admin/components/header.php
@@ -1,32 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="/stationery/app/resources/css/styles.css">
 </head>
+
 <body>
-<nav>
-    <ul class="head_menu">
-        <li>
-            <a class="head_link" href="home">Home</a>
-        </li>
-        <?php if (isset($_SESSION['user'])): ?>
-            <?php if ($_SESSION['user']['role'] == 'admin' || $_SESSION['user']['role'] == 'SuperAdmin'): ?>
-                <li>
-                    <a class="head_link" href="users">Moderate Users</a>
-                </li>
-                <li>
-                    <a class="head_link" href="category">Veiw categories</a>
-                </li>
-            <?php endif; ?>
+    <nav>
+        <ul class="head_menu">
+            <li>
+                <a class="head_link" href="../home">Site</a>
+            </li>
+            <li>
+                <a class="head_link" href="home">Dashboard</a>
+            </li>
+            <li>
+                <a class="head_link" href="users">Users</a>
+            </li>
+            <li>
+                <a class="head_link" href="category">Categories</a>
+            </li>
+            <li>
+                <a class="head_link" href="productCreating">Products</a>
+            </li>
+            <li>
+                <a class="head_link" href="reviews">Reviews</a>
+            </li>
             <li>
                 <a class="head_link" href="logout">Logout</a>
             </li>
-            <li>
-                <a href="reviews">reviews</a>
-            </li>
-        <?php endif; ?>
-    </ul>
-</nav>
+        </ul>
+    </nav>
