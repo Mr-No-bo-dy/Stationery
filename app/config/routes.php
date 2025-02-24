2c6afa792a2ffe0428cc988cb073ab665c0d757b
Mr-No-bo-dy <o.m.gutsalyuk@gmail.com>
2025-02-24 16:55:18 +0200

    small vendor updates

--- app/config/routes.php
+++ app/config/routes.php
@@ -40,18 +40,21 @@ $urlRoutes = [
     'admin/storeCategory' => 'admin/category/store',
     'admin/updateCategory' => 'admin/category/update',
     'admin/editCategory' => 'admin/category/edit',
+
     'admin/subcategory' => 'admin/subcategory/index',
     'admin/createSubcategory' => 'admin/subcategory/create',
     'admin/storeSubcategory' => 'admin/subcategory/store',
     'admin/updateSubcategory' => 'admin/subcategory/update',
     'admin/editSubcategory' => 'admin/subcategory/edit',
     'admin/deleteSubcategory' => 'admin/subcategory/delete',
+
     'admin/home' => 'admin/user/index',
     'admin/logout' => 'admin/user/logout',
     'admin/users' => 'admin/user/getAll',
     'admin/edit' => 'admin/user/edit',
     'admin/update' => 'admin/user/update',
     'admin/delete' => 'admin/user/delete',
+
     'admin/orders' => 'admin/order/index',
     'admin/ordersSorting' => 'admin/order/sorting',
     'admin/reviews' => 'admin/review/index',
