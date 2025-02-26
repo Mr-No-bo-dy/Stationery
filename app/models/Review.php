cfd579a91f4d323b5c8218da712aa17d0815769d
SmA1l2011 <sevadam766@gmail.com>
2025-02-26 19:03:36 +0200

    fix mini bugs

--- app/models/Review.php
+++ app/models/Review.php
@@ -15,22 +15,22 @@ class Review extends Model
         'is_active',
     ];
 
-    // this function get information about reviews and sort it by id or rating
+    // get information about reviews and sort it by id or rating
     public static function getSiteReviews($sortBy = "sort by id", $is_active = NULL): array
     {
         $sql = "SELECT r.user_id, u.name, r.rating, r.comment, r.id, r.product_id, r.is_active 
                 FROM reviews r 
                 JOIN users u ON r.user_id = u.id";
-        if ($is_active == "reviews only active") {
+        if ($is_active == "yes") {
             $sql .= " WHERE r.is_active = 1";
         }
-        if ($is_active == "reviews only not active") {
+        if ($is_active == "no") {
             $sql .= " WHERE r.is_active = 0";
         }
-        if ($sortBy == "sort by rating") {
+        if ($sortBy == "rating") {
             $sql .= " ORDER BY r.rating DESC";
         }
-        if ($sortBy == "sort by product id") {
+        if ($sortBy == "product id") {
             $sql .= " ORDER BY r.product_id DESC";
         }
         $stmt = self::builder()->prepare($sql);
@@ -38,7 +38,7 @@ class Review extends Model
         return $stmt->fetchAll();
     }
 
-    // this function get user id from userName
+    // get user id from userName
     public static function getUserId($userName)
     {
         $stmt = self::builder()->prepare("SELECT id FROM users WHERE name = :userName");
@@ -47,7 +47,7 @@ class Review extends Model
         return $stmt->fetch();
     }
 
-    // this function create reviews in data base 
+    // create reviews in data base 
     public static function createSiteReviews($product_id, $user_id, $rating, $comment)
     {
         $stmt = self::builder()->prepare("INSERT INTO `reviews`(`product_id`, `user_id`, `rating`, `comment`, `is_active`) VALUES (:product_id,:user_id,:rating,:comment,'0')");
@@ -58,7 +58,7 @@ class Review extends Model
         $stmt->execute();
     } 
 
-    // this function uapdate the value in is_active depending on the specified argument
+    // uapdate the value in is_active depending on the specified argument
     public static function approveReview($id, $value): void
     {
         $stmt = self::builder()->prepare("UPDATE reviews SET is_active = :value WHERE id = :id");
